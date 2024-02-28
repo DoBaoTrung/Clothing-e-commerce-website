86487003bda3a1e3b2aecc835ef3d9c33b9daf0d
DoBaoTrung <haiteenhp1@gmail.com>
2024-02-28 14:56:48 +0700

    upload source code

---
+++ app/Http/Controllers/ProductDetailController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Services\HomeService;
+use App\Services\ProductDetailService;
+
+class ProductDetailController extends Controller
+{
+    /**
+     * @var ProductDetailService
+     */
+    private $productDetailService;
+
+    /**
+     * ProductDetailService constructor.
+     *
+     * @param ProductDetailController $productDetailService
+     */
+    public function __construct(ProductDetailService $productDetailService)
+    {
+        $this->productDetailService = $productDetailService;
+    }
+    /**
+     * Displays home website.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show(Product $product) 
+    {
+        // dd($product);
+        return view('client.product-detail', $this->productDetailService->show($product));
+    }
+}
